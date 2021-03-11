dec2369ebff2e0524cb0cce44e9b07267df4e921
LeonDante-ctrl <maxgamerdu@gmail.com>
2021-03-11 09:10:15 +0300

    created a template for the database to reference later

---
+++ config.py
@@ -0,0 +1,17 @@
+from peewee import *
+from playhouse.sqlcipher_ext import SqlCipherDatabase
+
+# Defer initialization of the database until the script is executed from the
+# command-line.
+db = SqlCipherDatabase()
+
+class Entry(Model):
+    content = TextField()
+    timestamp = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = db
+
+def initialize(passphrase):
+    db.init('diary.db', passphrase=passphrase, kdf_iter=64000)
+    Entry.create_table()
